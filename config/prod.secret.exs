740cd81027a02849be9e7d038b8cc313bd548641
Phat Nguyen <tphat2616@gmail.com>
2021-07-01 15:13:21 +0700

    - config production env.

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,31 +4,21 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
-
 config :tanphat, Tanphat.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+  username: "postgres",
+  password: "09098237720aA@",
+  database: "tphat2616",
+  hostname: "51.79.165.79",
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
 
 config :tanphat, TanphatWeb.Endpoint,
-  http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [socket_opts: [:inet6]]
-  ],
-  secret_key_base: secret_key_base
+  server: true,
+  http: [scheme: "https", host: "tanphat.dev", port: 443],
+  secret_key_base: "VdEaZh0DFVDEeLHSzlKrMx5k/EmXSZPL1SUnUVo54SjfvvVf87FOcMFBS0KELNEF",
+  live_view: [signing_salt: "fOLDeUr4X"],
+  code_reloader: false,
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## Using releases (Elixir v1.9+)
 #

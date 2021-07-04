1aeabe6137417fcb0e3a5523ca41e74ef96e6848
Phat Nguyen <tphat2616@gmail.com>
2021-07-04 11:14:41 +0700

    - change metatag image.

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -29,7 +29,7 @@ config :tanphat, TanphatWeb.Endpoint,
   metatag: [
     title: "BITCOIN NEVA DIES",
     description: "Trang website lưu trữ thông tin",
-    image: "https://tanphat.dev/images/bitcoin_shortcut.png",
+    image: "https://tanphat.dev/images/bitcoin.svg",
     site: "https://tanphat.dev"
   ]
 

385bcef29384254094f8283b14885cd2c4227fa0
Phat Nguyen <tphat2616@gmail.com>
2021-08-07 22:26:35 +0700

    something.

--- lib/tanphat_web/live/crytocurrency/eth_burned.ex
+++ lib/tanphat_web/live/crytocurrency/eth_burned.ex
@@ -4,11 +4,16 @@ defmodule TanphatWeb.Cryptocurrency.EthBurned do
 
   def handle_params(_params, _uri, socket) do
     eth_burned = ApiService.eth_burned()
-    total_burned = eth_burned["Total burned"]
     eth_price = ApiService.eth_price()
+    total_burned = eth_burned["Total burned"]
     price = eth_price["price"]
+
+    total_burned = String.split(total_burned, ",")
+    total_burned = Enum.reduce(total_burned, fn(x, sum) -> "#{sum}#{x}" end)
     total = String.to_float(total_burned) * price
     money = total |> trunc()
+
+    total_burned = Number.Currency.number_to_currency(total_burned, [unit: "", delimiter: ","])
     total_money = Number.Currency.number_to_currency(money)
     {:noreply, assign(socket, total_burned: total_burned, total_money: total_money)}
   end

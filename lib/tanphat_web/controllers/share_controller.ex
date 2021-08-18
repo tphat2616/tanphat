508e683d974a6e1f42546c4ef95e19113b7a63d1
Phat Nguyen <tphat2616@gmail.com>
2021-08-18 12:55:03 +0700

    - add page title for share controller.

--- lib/tanphat_web/controllers/share_controller.ex
+++ lib/tanphat_web/controllers/share_controller.ex
@@ -2,23 +2,23 @@ defmodule TanphatWeb.ShareController do
     use TanphatWeb, :controller
   
     def cryto_market(conn, _opts) do
-      render(conn, "cryto_market.html")
+      render(conn, "cryto_market.html", page_title: "Crypto Market")
     end
 
     def chart(conn, _opts) do
-      render(conn, "trading_view.html")
+      render(conn, "trading_view.html", page_title: "Trading View Chart")
     end
 
     def market(conn, _opts) do
-      render(conn, "market_data.html")
+      render(conn, "market_data.html", page_title: "Market Data")
     end
 
     def forex_cross_rate(conn, _opts) do
-      render(conn, "forex_cross_rate.html")
+      render(conn, "forex_cross_rate.html", page_title: "Forex Cross Rates")
     end
 
     def forex_heat(conn, _opts) do
-      render(conn, "forex_heatmap.html")
+      render(conn, "forex_heatmap.html", page_title: "Forex Heatmap")
     end
   end
   
